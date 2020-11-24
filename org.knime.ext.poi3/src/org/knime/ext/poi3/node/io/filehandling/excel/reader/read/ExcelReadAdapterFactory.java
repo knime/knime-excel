22e9e203be9519d71f77a6102f937006799d6be0
Simon Schmid <simon.schmid@knime.com>
2020-11-24 15:08:18 +0100

    Use SupplierCellValueProducerFactory class of core

--- org.knime.ext.poi3/src/org/knime/ext/poi3/node/io/filehandling/excel/reader/read/ExcelReadAdapterFactory.java
+++ org.knime.ext.poi3/src/org/knime/ext/poi3/node/io/filehandling/excel/reader/read/ExcelReadAdapterFactory.java
@@ -65,6 +65,7 @@ import org.knime.core.data.convert.map.MappingFramework;
 import org.knime.core.data.convert.map.PrimitiveCellValueProducer;
 import org.knime.core.data.convert.map.ProducerRegistry;
 import org.knime.core.data.convert.map.SimpleCellValueProducerFactory;
+import org.knime.core.data.convert.map.SupplierCellValueProducerFactory;
 import org.knime.core.data.def.BooleanCell;
 import org.knime.core.data.def.DoubleCell;
 import org.knime.core.data.def.IntCell;
